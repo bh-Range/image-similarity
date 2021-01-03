3cd469f953a43a9f52260213ec837939b0196e87
Range <48876799+bh-Range@users.noreply.github.com>
2021-01-03 09:17:04 +0800

    Update model_util.py

--- model_util.py
+++ model_util.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from tensorflow.python.keras.applications.mobilenet import MobileNet, preprocess_input
 from tensorflow.python.keras.preprocessing import image as process_image
-from tensorflow.python.keras.utils import Sequence
+from tensorflow.keras.utils import Sequence
 from tensorflow.python.keras.layers import GlobalAveragePooling2D
 from tensorflow.python.keras import Model
 
